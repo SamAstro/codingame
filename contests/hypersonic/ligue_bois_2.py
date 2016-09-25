bd1458fa42548b2f2a40db7a0b39c2298f0643fb
Samia Drappeau <drappeau.samia@gmail.com>
2016-09-25 02:15:06 +0200

    Bot of the 'Hypersonic' ligue bois 2 contest.

---
+++ contests/hypersonic/ligue_bois_2.py
@@ -0,0 +1,142 @@
+#!/opt/local/bin/python
+"""
+BOT FOR LIGUE BOIS 2 OF HYPERSONIC CONTEST
+
+Version:    1.1
+Created:    09/25/2016
+Compiler:   python3.5
+
+Author: Dr. Samia Drappeau (SD), drappeau.samia@gmail.com
+Notes: 
+"""
+import sys
+import math
+import numpy as np
+
+
+## Functions
+
+def find_crates(idx, hero):
+    ndim = len(idx[0])
+    cx, cy = None, None
+    
+    min_dist = 1.e5
+    #mdx = 1.e5
+    #mdy = 1.e5
+    
+    for n in range(ndim):
+        dx,dy = distance(hero, idx[1][n], idx[0][n])
+        dist = math.sqrt(dx**2 + dy**2)
+        if dist < min_dist:
+            #if dx < mdx:
+            cx = idx[1][n]
+                #mdx = cx
+            #if dy < mdy:
+            cy = idx[0][n]
+                #mdy = cy
+            min_dist = dist
+    return cx, cy
+    
+    
+def distance(hero, cx, cy):
+    distx = math.sqrt((cx - hero.x)**2)
+    disty = math.sqrt((cy - hero.y)**2)
+    return distx, disty
+
+# Creating Class entity
+class entity:
+    def __init__(self, entity_type, owner, x, y, param_1, param_2):
+        self.entity_type = entity_type
+        self.owner = owner
+        self.x = int(x)
+        self.y = int(y)
+        self.param_1 = param_1
+        self.param_2 = param_2
+    
+
+def main(argv):
+    # Dictionary saving the entities
+    entity_dict = {}
+
+    # Variable saving my index in the dictionary
+    myidx = None
+    oidx = None
+    # Bomb reach initialized to 3 squares
+    bomb_reach = 3
+
+    # Offset
+    offset = 5
+
+    # Auto-generated code below aims at helping you parse
+    # the standard input according to the problem statement.
+
+    width, height, my_id = [int(i) for i in input().split()]
+    print("my id:", my_id, file=sys.stderr)
+
+    # Saving the grid in a matrix
+    grid = np.array([['.' for j in range(width)] for i in range(height)])
+
+    # game loop
+    while True:
+        for i in range(height):
+            row = input()
+            # Populating the grid
+            for j in range(width):
+                grid[i,j] = row[j]
+                
+        # Find the indices of all left crates
+        idx = np.where(grid != ".")
+        
+        # Entities
+        entities = int(input())
+        for i in range(entities):
+            entity_type, owner, x, y, param_1, param_2 = [int(j) for j in input().split()]
+            # Populating the entity dictionary
+            entity_dict[i] = entity(entity_type, owner, x, y, param_1, param_2)
+            if entity_type == 0 and owner == my_id:
+                myidx = i
+            if entity_type == 2:
+                dohx, dohy = distance(entity_dict[myidx], x, y)
+                doh = math.sqrt(dohx**2 + dohy**2)
+                print(x,y,dohx,dohy, file=sys.stderr)
+                if (dohx < 1 or dohy < 1) and doh < 2:
+                    oidx = i
+                else:
+                    oidx = None
+            if entity_type == 1 and owner == my_id:
+                bomb_reach = param_2-1
+                
+        if oidx == None:
+            print("moving...", file=sys.stderr)
+            # Write an action using print
+            # To debug: print("Debug messages...", file=sys.stderr)
+            
+            # Find which crate is closest to me
+            cx, cy = find_crates(idx, entity_dict[myidx])
+            
+            distx, disty = distance(entity_dict[myidx], cx, cy)
+            
+            print(entity_dict[myidx].x, entity_dict[myidx].y, cx, cy, distx, disty, sep=" ", file=sys.stderr)
+            
+            if (disty < bomb_reach or distx < bomb_reach):
+                if entity_dict[myidx].param_1 != 0:
+                    # Bomb closest crate
+                    print("BOMB", cx, cy, sep=" ")
+                else:
+                    if (entity_dict[myidx].x+offset > width-1) or (entity_dict[myidx].y+offset > height-1):
+                        print("MOVE", entity_dict[myidx].x-5, entity_dict[myidx].y-5, sep=" ")
+                    else:
+                        print("MOVE", entity_dict[myidx].x+offset, entity_dict[myidx].y+offset, sep=" ")
+            else:
+                # Move to the closest crate
+                print("MOVE", cx, cy, sep=" ")
+        else:
+            print("UPs! UPs! UPs!", file=sys.stderr)
+            print("MOVE", entity_dict[oidx].x, entity_dict[oidx].y, sep=" ")
+            oidx = None
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
+
