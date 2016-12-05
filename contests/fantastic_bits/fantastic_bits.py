8fb8db46ed152f39fa94777f8ba45252341ee3ba
Samia Drappeau <drappeau.samia@gmail.com>
2016-12-05 12:42:54 +0100

    Final version of my FB bot.

--- contests/fantastic_bits/fantastic_bits.py
+++ contests/fantastic_bits/fantastic_bits.py
@@ -1,5 +1,5 @@
 """
-DESCRIPTION OF THE SCRIPT
+BOT FOR THE 'FANTASTIC BITS' [CG] CONTEST
 
 Version:
 Created:
@@ -23,6 +23,20 @@ def distance(wiz, entity):
     dist = dx + dy
     return dist
 
+def findFarthest(wiz, entities, used_entities=[]):
+    ''' Find closest entity to my wizard.'''
+    max_distance = sys.float_info.max
+    farthest_entity = None
+    for entity_id, entity in entities.items():
+        if entities[entity_id].entity_id in used_entities:
+            continue
+        else:
+            dist = distance(wiz, entity)
+            if farthest_entity == None or dist > max_distance:
+                max_distance = dist
+                farthest_entity = entities[entity_id]
+    return farthest_entity
+
 def findClosest(wiz, entities, used_entities=[]):
     ''' Find closest entity to my wizard.'''
     min_distance = 0
@@ -45,7 +59,22 @@ def is_entities_blocking(wiz, closest_snaffle, entities, my_team_id):
     ''' Are the entities, the wizard and the closest_snaffle on the same line?'''
     entity_blocking = False
     for entity_id, entity in entities.items():
-        are_wiz_snaf_entity_collinear = collinear(wiz, closest_snaffle, entity)
+        if (entity.entity_type == closest_snaffle.entity_type and entity.entity_id == closest_snaffle.entity_id) or (entity.entity_type == wiz.entity_type and entity.entity_id == wiz.entity_id):
+            continue
+        else:
+            are_wiz_snaf_entity_collinear = collinear(wiz, closest_snaffle, entity)
+            dx = wiz.x - entity.x if my_team_id == 1 else entity.x - wiz.x
+            if are_wiz_snaf_entity_collinear and dx > 0:
+                entity_blocking = True
+            else:
+                continue
+    return entity_blocking
+
+def is_entities_ontheway(wiz, opp_goal, entities, my_team_id):
+    ''' Are the entities, the wizard and the closest_snaffle on the same line?'''
+    entity_blocking = False
+    for entity_id, entity in entities.items():
+        are_wiz_snaf_entity_collinear = collinear(wiz, opp_goal, entity)
         dx = wiz.x - entity.x if my_team_id == 1 else entity.x - wiz.x
         
         if are_wiz_snaf_entity_collinear and dx > 0:
@@ -180,6 +209,7 @@ class Wizard(Entity):
     to_snaf = 1e9
     snafid = None
     prev_action = None
+    msg = None
 
     def __init__(self, entity_id, entity_type, x, y, vx, vy, state):
         super().__init__(entity_id, entity_type, x, y, vx, vy, state)
@@ -195,9 +225,32 @@ class Wizard(Entity):
             self.cast_spell(self.action, self.snafid)
             game.set_magic(spell_cost = spell_cost)
         else:
-            print(self.action, self.DEST[0], self.DEST[1], self.power, sep=" ")
+            if self.msg == None:
+                print(self.action, self.DEST[0], self.DEST[1], self.power, sep=" ")
+            else:
+                print(self.action, self.DEST[0], self.DEST[1], self.power, self.msg, sep=" ")
 
+    def throw_action(self, opp_goal):
+        self.power = '500'
+        self.action = 'THROW'
+        self.DEST = [opp_goal.x, opp_goal.y]
 
+    def move_action(self, entity):
+        self.power = '150'
+        self.action = 'MOVE'
+        self.DEST = [entity.x, entity.y]
+
+    def find_best_snaffle_to_flipendo(self, snaffles, opp_wizards, wizards, bludgers, opp_goal, my_team_id):
+        for snafid, snaf in snaffles.items():
+            target_aligned = is_entities_aligned(wiz, snaf, opp_goal)
+            bludger_blocking = is_entities_blocking(wiz, snaf, bludgers, my_team_id)
+            snaffle_blocking = is_entities_blocking(wiz, snaf, snaffles, my_team_id)
+            mywiz_blocking = is_entities_blocking(wiz, snaf, wizards, my_team_id)
+            opp_wiz_blocking = is_entities_blocking(wiz, snaf, opp_wizards, my_team_id)
+            if is_snaffle_infront(wiz, snaf, my_team_id) and target_aligned and (not bludger_blocking) and (not snaffle_blocking) and (not mywiz_blocking) and (not opp_wiz_blocking):
+                return snaf
+            else:
+                return None
 
 
 class Bludger(Entity):
@@ -211,8 +264,6 @@ class Goal():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
-
 
 
 # Grab Snaffles and try to throw them through the opponent's goal!
@@ -273,132 +324,43 @@ while True:
         else:
             print("Wrong type of entity", file=sys.stderr)
 
+
     used_snaffle = []
     spell_cost = 0
 
     for wizid, wiz in game.wizard.items():
-        closest_snaffle = findClosest(wiz, game.snaffle, used_snaffle)
-        if wizid == 0:
-            ''' First wizard will play defence...'''
-            if game.nsnaffle == 0:
-                ''' ... by attacking opponent.'''
-                if game.mana_lvl > 20:
-                    wiz.DEST = [-99, -99]
-                    wiz.action = 'FLIPENDO'
-                    spell_cost = 20
-                    wiz.snafid = game.opp_wizard[random.randint(0,1)].entity_id
-                else:
-                    if wiz.state == 0:
-                        wiz.action = "MOVE"
-                        # Find random opp to bump into
-                        tmp = random.randint(0,1)
-                        wiz.DEST = [game.opp_wizard[tmp].x, game.opp_wizard[tmp].y]
-                        wiz.power = '150'
-                    else:
-                        wiz.action = "THROW"
-                        wiz.DEST = [opp_goal.x, opp_goal.y]
-                        wiz.power = '500'
+        best_snaffle = wiz.find_best_snaffle_to_flipendo(game.snaffle, game.opp_wizard, game.wizard, game.bludger, opp_goal, my_team_id)
+        farthest_snaffle = findFarthest(wiz, game.snaffle, used_snaffle)
+        dist_wiz_snaffle = distance(wiz, farthest_snaffle)
 
-            elif game.nsnaffle < 20 or turn < 10:
-                ''' ... by actually been a field player!'''
-                target_aligned = is_entities_aligned(wiz, closest_snaffle, opp_goal)
-                bludger_blocking = is_entities_blocking(wiz, closest_snaffle, game.bludger, my_team_id)
-                snaffle_blocking = is_entities_blocking(wiz, closest_snaffle, game.snaffle, my_team_id)
-                mywiz_blocking = is_entities_blocking(wiz, closest_snaffle, game.wizard, my_team_id)
-                opp_wiz_blocking = is_entities_blocking(wiz, closest_snaffle, game.opp_wizard, my_team_id)
-                dist_wiz_snaffle = distance(wiz, closest_snaffle)
-
-                if wiz.state == 1:
-                    wiz.action = 'THROW'
-                    wiz.DEST = [opp_goal.x, opp_goal.y]
-                    wiz.power = '500'
-                elif game.mana_lvl >= 20 and is_snaffle_infront(wiz, closest_snaffle, my_team_id) and target_aligned and (not bludger_blocking) and (not snaffle_blocking) and (not mywiz_blocking) and (not opp_wiz_blocking):
-                    wiz.DEST = [-99, -99]
-                    wiz.action = 'FLIPENDO'
-                    spell_cost = 20
-                    wiz.snafid = closest_snaffle.entity_id
-                    used_snaffle.append(closest_snaffle.entity_id)
-                elif game.mana_lvl >= 20 and is_snaffle_behind(wiz, closest_snaffle, my_team_id) and dist_wiz_snaffle > 2000*2000:
-                    wiz.DEST = [-99, -99]
-                    wiz.action = 'ACCIO'
-                    spell_cost = 20
-                    wiz.snafid = closest_snaffle.entity_id
-                    used_snaffle.append(closest_snaffle.entity_id)
-                else:
-                    wiz.action = 'MOVE'
-                    wiz.DEST = [closest_snaffle.x, closest_snaffle.y]
-                    used_snaffle.append(closest_snaffle.entity_id)
-                    wiz.power = '150'
-                
-            else:
-                # Goalkeeper
-                if wiz.state == 1:
-                    wiz.action = "THROW"
-                    wiz.DEST = [opp_goal.x, opp_goal.y]
-                    wiz.power = '500'
-                else:
-                    dist_gk_goal = distance(wiz, my_goal)
-                    if dist_gk_goal > 5000*5000:
-                        print('Moving back home', file=sys.stderr)
-                        wiz.action = 'MOVE'
-                        wiz.DEST = [my_goal.x, my_goal.y]
-                        wiz.power = 150
-                    else:
-                        # Where are the snaffles? Are they too close to home?
-                        # if snaffle too close to own goal, try to catch it,
-                        # else move back in position.
-                        closest_snaffle_to_home = findClosest(my_goal, game.snaffle, used_snaffle)
-
-                        # Trying to cast spell on it or else catch it.
-                        if game.mana_lvl > 20 and is_snaffle_infront(wiz, closest_snaffle_to_home, my_team_id):
-                            wiz.DEST = [-99, -99]
-                            wiz.action = 'FLIPENDO'
-                            wiz.snafid = closest_snaffle_to_home.entity_id
-                            spell_cost = 20
-                        else:
-                            wiz.action = 'MOVE'
-                            wiz.DEST = [closest_snaffle_to_home.x, closest_snaffle_to_home.y]
-                            wiz.power = 150
-                        used_snaffle.append(closest_snaffle_to_home.entity_id)
-
-        else:
-            ''' Second wizard always attacks!'''
-            target_aligned = is_entities_aligned(wiz, closest_snaffle, opp_goal)
-            bludger_blocking = is_entities_blocking(wiz, closest_snaffle, game.bludger, my_team_id)
-            snaffle_blocking = is_entities_blocking(wiz, closest_snaffle, game.snaffle, my_team_id)
-            mywiz_blocking = is_entities_blocking(wiz, closest_snaffle, game.wizard, my_team_id)
-            opp_wiz_blocking = is_entities_blocking(wiz, closest_snaffle, game.opp_wizard, my_team_id)
-            dist_wiz_snaffle = distance(wiz, closest_snaffle)
-            dist_goal_snaffle = distance(opp_goal, closest_snaffle)
-
-            if wiz.state == 1:
-                wiz.action = 'THROW'
-                wiz.DEST = [opp_goal.x, opp_goal.y]
-                wiz.power = '500'
-            elif game.mana_lvl >= 20 and is_snaffle_infront(wiz, closest_snaffle, my_team_id) and target_aligned and (not bludger_blocking) and (not snaffle_blocking) and (not mywiz_blocking) and (not opp_wiz_blocking): 
+        if wiz.state == 1:
+            wiz.throw_action(opp_goal)
+        elif game.mana_lvl >= 20 and best_snaffle != None:
                 wiz.DEST = [-99, -99]
                 wiz.action = 'FLIPENDO'
                 spell_cost = 20
-                wiz.snafid = closest_snaffle.entity_id
-                used_snaffle.append(closest_snaffle.entity_id)
-            elif game.mana_lvl >= 20 and is_snaffle_behind(wiz, closest_snaffle, my_team_id) and dist_wiz_snaffle > 2000*2000:
-                wiz.DEST = [-99, -99]
+                wiz.snafid = best_snaffle.entity_id
+                used_snaffle.append(best_snaffle.entity_id)
+        elif game.mana_lvl >= 20 and is_snaffle_behind(wiz, farthest_snaffle, my_team_id) and dist_wiz_snaffle > 2000*2000:
+            wiz.DEST = [-99, -99]
+            if game.nsnaffle == 1:
+                wiz.action = 'PETRIFICUS'
+                spell_cost = 10
+            else:
                 wiz.action = 'ACCIO'
                 spell_cost = 20
-                wiz.snafid = closest_snaffle.entity_id
-                used_snaffle.append(closest_snaffle.entity_id)
-            else:
-                wiz.action = 'MOVE'
-                wiz.DEST = [closest_snaffle.x, closest_snaffle.y]
-                used_snaffle.append(closest_snaffle.entity_id)
-                wiz.power = '150'
+            wiz.snafid = farthest_snaffle.entity_id
+            used_snaffle.append(farthest_snaffle.entity_id)
+        else:
+            # find closest snaffle to move to.
+            closest_snaffle = findClosest(wiz, game.snaffle, used_snaffle)
+            wiz.move_action(closest_snaffle)
+            used_snaffle.append(closest_snaffle.entity_id)
 
-        if game.nsnaffle == 1:
+
+        if game.nsnaffle <= 2:
             used_snaffle = []
 
-
         wiz.play(spell_cost)
-
+        
     game.set_magic(turn_magic_pt=1)
-
-
